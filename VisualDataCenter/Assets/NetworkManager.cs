b9c92f1dd70d85a91201e66fb52ca3e3efad62e6
Sui <vsui2@illinois.edu>
2018-11-10 18:26:33 -0600

    Router generation prototype

---
+++ VisualDataCenter/Assets/NetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+    public GameObject routerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		// Connect to server
+	}
+
+    void CreateRouter()
+    {
+        GameObject router = Instantiate(routerPrefab, transform.position, Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+        {
+            CreateRouter();
+        }
+	}
+}
