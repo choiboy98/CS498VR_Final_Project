e3837cb32f2ce969885e36e6de5a8198beb688ec
Sui <vsui2@illinois.edu>
2018-11-27 16:03:20 -0600

    ListNodes function

--- VisualDataCenter/Assets/NetworkManager.cs
+++ VisualDataCenter/Assets/NetworkManager.cs
@@ -6,15 +6,56 @@ using UnityEngine.Networking;
 public class NetworkManager : MonoBehaviour {
     public GameObject routerPrefab;
     public GameObject switchPrefab;
+    private string address = "http://192.168.56.1:3080/v2";
+    private string projectId = "836c3bbb-6aa6-4817-8c73-4697a1946d4e";
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		// Connect to server
 	}
 
-    void CreateRouter()
+    IEnumerator CreateRouter()
     {
-        GameObject router = Instantiate(routerPrefab, transform.position + transform.forward, Quaternion.identity);
+        string postData = @"{""compute_id"": ""vm"",""x"": 10,""y"": 10}";
+        string url = "http://192.168.56.1:3080/v2/projects/836c3bbb-6aa6-4817-8c73-4697a1946d4e/appliances/1966b864-93e7-32d5-965f-001384eec461";
+
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(postData);
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        Debug.Log("Sent!");
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+        else
+        {
+            GameObject router = Instantiate(routerPrefab, transform.position + transform.forward, Quaternion.identity);
+        }
+    }
+
+    IEnumerator ListNodes()
+    {
+        string url = address + "/projects/" + projectId + "/nodes";
+        Debug.Log("GET " + url);
+
+        var request = new UnityWebRequest(url, "GET");
+
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.downloadHandler.text);
+        } else
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+        // TODO parse JSON to get the node ids
     }
 
     IEnumerator CreateSwitch()
@@ -49,6 +90,10 @@ public class NetworkManager : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.B))
         {
             StartCoroutine(CreateSwitch());
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(ListNodes());
         }
 	}
 }
