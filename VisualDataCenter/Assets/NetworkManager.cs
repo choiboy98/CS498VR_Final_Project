a9cf9913452715457cd91af23702044c6ed0a7ca
Sui <vsui2@illinois.edu>
2018-11-27 14:43:50 -0600

    Properly create switches on GNS3

--- VisualDataCenter/Assets/NetworkManager.cs
+++ VisualDataCenter/Assets/NetworkManager.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
-
 public class NetworkManager : MonoBehaviour {
     public GameObject routerPrefab;
     public GameObject switchPrefab;
@@ -19,17 +19,24 @@ public class NetworkManager : MonoBehaviour {
 
     IEnumerator CreateSwitch()
     {
-        GameObject switch_ = Instantiate(switchPrefab, transform.position + transform.forward, Quaternion.identity);
         string postData = @"{""compute_id"": ""vm"",""x"": 10,""y"": 10}";
-        Debug.Log(postData);
-        // Project ID might need to be changed
-        UnityWebRequest post = UnityWebRequest.Post("http://192.17.11.161:3080/v2/projects/3c6171ad-26b2-4af9-91f1-742c5a8eee34/appliances/1966b864-93e7-32d5-965f-001384eec461", postData);
-        post.SetRequestHeader("Content-Type", "application/json");
-        yield return post.SendWebRequest();
-        if (post.isNetworkError || post.isHttpError)
+        string url = "http://192.168.56.1:3080/v2/projects/836c3bbb-6aa6-4817-8c73-4697a1946d4e/appliances/1966b864-93e7-32d5-965f-001384eec461";
+
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(postData);
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        Debug.Log("Sent!");
+        if (request.isNetworkError || request.isHttpError)
         {
-            Debug.Log(post.error);
-            Debug.Log(post.downloadHandler.text);
+            Debug.Log(request.downloadHandler.text);
+        } else
+        {
+            GameObject switch_ = Instantiate(switchPrefab, transform.position + transform.forward, Quaternion.identity);
         }
     }
 	
