049ce35e3e12ce830a406588f98c6e9662be30b0
Daniel S Choi <daniel.won.choi@gmail.com>
2018-12-17 03:02:36 -0600

    integrating thomas and michaels project

---
+++ VisualDataCenter/Assets/server.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class server : MonoBehaviour {
+
+    public bool highlight;
+    public GameObject hand;
+    public Renderer mesh;
+    Color temp;
+
+    // Use this for initialization
+    void Start () {
+        BoxCollider boxCollider = gameObject.GetComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+        hand = GameObject.Find("/OVRPlayerController/OVRCameraRig/TrackingSpace/RightHandAnchor");
+        highlight = false;
+        mesh = GameObject.Find("panel_for_server").GetComponent<MeshRenderer>();
+        temp = mesh.material.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (highlight)
+        {
+            mesh.material.color = Color.yellow;
+        }
+        else mesh.material.color = temp; 
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == hand) highlight = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == hand) highlight = false;
+    }
+}
